51dcc401e967947545d235392bd490fb200c6822
James E. Stine <james.stine@okstate.edu>
2024-05-05 00:40:01 -0500

    odd rounding?

--- fptests/create_vectors.sh
+++ fptests/create_vectors.sh
@@ -7,9 +7,11 @@ $BUILD/testfloat_gen -tininessafter -level 1 -rminMag f32_div > $OUTPUT/f32_div_
 $BUILD/testfloat_gen -tininessafter -level 1 -rmax f32_div > $OUTPUT/f32_div_ru.tv
 $BUILD/testfloat_gen -tininessafter -level 1 -rmin f32_div > $OUTPUT/f32_div_rd.tv
 $BUILD/testfloat_gen -tininessafter -level 1 -rnear_maxMag f32_div > $OUTPUT/f32_div_rnm.tv
+$BUILD/testfloat_gen -tininessafter -level 1 -rodd f32_div > $OUTPUT/f32_div_odd.tv
 echo "Creating f32_sqrt vectors"
 $BUILD/testfloat_gen -tininessafter -level 2 -rnear_even f32_sqrt > $OUTPUT/f32_sqrt_rne.tv
 $BUILD/testfloat_gen -tininessafter -level 2 -rminMag f32_sqrt > $OUTPUT/f32_sqrt_rz.tv
 $BUILD/testfloat_gen -tininessafter -level 2 -rmax f32_sqrt > $OUTPUT/f32_sqrt_ru.tv
 $BUILD/testfloat_gen -tininessafter -level 2 -rmin f32_sqrt > $OUTPUT/f32_sqrt_rd.tv
-$BUILD/testfloat_gen -tininessafter -level 2 -rnear_maxMag f32_sqrt > $OUTPUT/f32_sqrt_rnm.tv
+$BUILD/testfloat_gen -tininessafter -level 2 -rnear_maxMag f32_sqrt > $OUTPUT/f32_sqrt_rnm.t
+$BUILD/testfloat_gen -tininessafter -level 2 -rodd f32_sqrt > $OUTPUT/f32_sqrt_odd.tv
--- fptests/remove_spaces.sh
+++ fptests/remove_spaces.sh
@@ -7,9 +7,11 @@ sed -i 's/ /_/g' $OUTPUT/f32_div_rz.tv
 sed -i 's/ /_/g' $OUTPUT/f32_div_ru.tv
 sed -i 's/ /_/g' $OUTPUT/f32_div_rd.tv
 sed -i 's/ /_/g' $OUTPUT/f32_div_rnm.tv
+sed -i 's/ /_/g' $OUTPUT/f32_div_odd.tv
 echo "Editing f32_sqrt test vectors"
 sed -i 's/ /_/g' $OUTPUT/f32_sqrt_rne.tv
 sed -i 's/ /_/g' $OUTPUT/f32_sqrt_rz.tv
 sed -i 's/ /_/g' $OUTPUT/f32_sqrt_ru.tv
 sed -i 's/ /_/g' $OUTPUT/f32_sqrt_rd.tv
 sed -i 's/ /_/g' $OUTPUT/f32_sqrt_rnm.tv
+sed -i 's/ /_/g' $OUTPUT/f32_sqrt_odd.tv
